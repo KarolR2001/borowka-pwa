516592c8009f4d174b10d6098ec663ebb24d9b09
KarolR2001 <karolrembiasz@o2.pl>
2026-08-10 11:53:48 +0200

    test: provide scroll api in jsdom

--- src/testing/setupTests.ts
+++ src/testing/setupTests.ts
@@ -1,1 +1,9 @@
 import "@testing-library/jest-dom/vitest";
+
+if (!Element.prototype.scrollIntoView) {
+  Object.defineProperty(Element.prototype, "scrollIntoView", {
+    configurable: true,
+    value: () => undefined,
+    writable: true
+  });
+}
